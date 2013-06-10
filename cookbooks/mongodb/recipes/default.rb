42f7468b20597da7d7c44398e4cf030222c24f34
sureshchef <suresh.cheftest@gmail.com>
2013-06-10 13:07:54 +0000

    mongo default

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -22,18 +22,6 @@
 package "mongodb" do
   action :install
 end
-
-needs_mongo_gem = (node.recipes.include?("mongodb::replicaset") or node.recipes.include?("mongodb::mongos"))
-
-if needs_mongo_gem
-  # install the mongo ruby gem at compile time to make it globally available
-  gem_package 'mongo' do
-    action :nothing
-  end.run_action(:install)
-  Gem.clear_paths
-end
-
-if node.recipes.include?("mongodb::default") or node.recipes.include?("mongodb")
   # configure default instance
   mongodb_instance "mongodb" do
     mongodb_type "mongod"
