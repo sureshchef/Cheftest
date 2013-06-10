47f3576aa6251e2fb22f8803a041caaf61776399
sureshchef <suresh.cheftest@gmail.com>
2013-06-10 13:35:48 +0000

    Mongo Commit

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -23,6 +23,26 @@ package "mongodb" do
   action :install
 end
 
+needs_mongo_gem = (node.recipes.include?("mongodb::replicaset") or node.recipes.include?("mongodb::mongos"))
+
+if needs_mongo_gem
+  # install the mongo ruby gem at compile time to make it globally available
+  gem_package 'mongo' do
+    action :nothing
+  end.run_action(:install)
+  Gem.clear_paths
+end
+
+if node.recipes.include?("mongodb::default") or node.recipes.include?("mongodb")
+  # configure default instance
+  mongodb_instance "mongodb" do
+    mongodb_type "mongod"
+    port         node['mongodb']['port']
+    logpath      node['mongodb']['logpath']
+    dbpath       node['mongodb']['dbpath']
+  end
+end
+
 ## Firewall configuration ##
 #
 # in order to find all member nodes of a mongodb cluster you have to run queries
