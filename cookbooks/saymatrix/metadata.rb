f7173508ae68317ba72bf3cd64ed71ef925a1d88
sureshchef <suresh.cheftest@gmail.com>
2013-06-11 08:58:28 +0000

    SayMatrix Commitjava

--- cookbooks/saymatrix/metadata.rb
+++ cookbooks/saymatrix/metadata.rb
@@ -1,7 +1,19 @@
-name             'saymatrix'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures saymatrix'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+nme              "java"
+maintainer        "Opscode, Inc."
+maintainer_email  "cookbooks@opscode.com"
+license           "Apache 2.0"
+description       "Installs Java runtime."
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.11.4"
+
+recipe "java", "Installs Java runtime"
+recipe "java::openjdk", "Installs the OpenJDK flavor of Java"
+recipe "java::oracle", "Installs the Oracle flavor of Java"
+recipe "java::oracle_i386", "Installs the 32-bit jvm without setting it as the default"
+
+
+%w{ debian ubuntu centos redhat scientific fedora amazon arch oracle freebsd windows }.each do |os|
+  supports os
+end
+
+depends "windows"
