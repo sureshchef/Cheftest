63e41d2acc29321d177812b9ab9159d0dbcf0681
sureshchef <suresh.cheftest@gmail.com>
2013-06-14 08:05:56 +0000

    remove

--- cookbooks/java/recipes/default.rb
+++ cookbooks/java/recipes/default.rb
@@ -27,3 +27,4 @@ include_recipe "java::#{node['java']['install_flavor']}"
     only_if { node['java']['remove_deprecated_packages'] }
   end
 end
+
--- knife.rb
+++
@@ -1,8 +0,0 @@
-log_level                :info
-log_location             STDOUT
-node_name                'ubuntu'
-client_key               '/root/.chef/ubuntu.pem'
-validation_client_name   'chef-validator'
-validation_key           '/root/.chef/chef-validator.pem'
-chef_server_url          'https://54.217.219.134'
-syntax_check_cache_path  '/root/.chef/syntax_check_cache'
