db44bcd519924c8d317b782fc34e960c9af19c59
sureshchef <suresh.cheftest@gmail.com>
2013-06-15 11:01:37 +0000

    fastflow commit

--- cookbooks/fastflow/recipes/default.rb
+++ cookbooks/fastflow/recipes/default.rb
@@ -20,3 +20,11 @@ package "autoconf"
 package "autogen" if platform_family?("debian")
 package "gtar" if platform?("freebsd")
 
+ark "fastflow" do
+  version "2.0.0"
+  url 'https://github.com/hrijulp/fastflow/blob/master/fastflow-2.0.0.tar.gz?raw=true'
+  checksum '5996e676f17457c823d86f1605eaa44ca8a81e70d6a0e5f8e45b51e62e0c52e8'
+  append_env_path true
+  action :install
+end
+
