dd0ab65c2556a4e1112669896e4e349aec3dc521
sureshchef <suresh.cheftest@gmail.com>
2013-06-17 09:32:09 +0000

    saymetrix CB

---
+++ cookbooks/saymetrix/metadata.rb
@@ -0,0 +1,16 @@
+name             'saymetrix'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures saymetrix'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+
+depends "apt"
+depends "java"
+depends "build-essential"
+depends "openssl"
+depends "ark"
+depends "subversion"
+depends "application"
