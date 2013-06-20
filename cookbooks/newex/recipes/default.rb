a806029b288b256b3a3adaa2d8c650aa81de7b3e
sureshchef <suresh.cheftest@gmail.com>
2013-06-20 12:45:47 +0000

    new

--- cookbooks/newex/recipes/default.rb
+++ cookbooks/newex/recipes/default.rb
@@ -28,7 +28,7 @@ end
 
 ark "metrix" do
   version "1.0.0"
-  url 'https://www.dropbox.com/s/jfdwb4rfeklwuqv/saymetrixApp.zip?raw=true'
+  url 'http://www.dropbox.com/s/jfdwb4rfeklwuqv/saymetrixApp.zip?raw=true'
   action :install
 end
 
