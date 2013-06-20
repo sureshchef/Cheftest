f1cb65ffcb29504096f492c06d7a4173ed70a6bf
sureshchef <suresh.cheftest@gmail.com>
2013-06-20 08:41:30 +0000

    newex

---
+++ cookbooks/newex/recipes/default.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: newex
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+include_recipe "apt"
+include_recipe "java"
+include_recipe "openssl"
+include_recipe "ark"
+include_recipe "apache"
+
+package "unzip"
+package "gtar" if platform?("freebsd")
+
+ark "play" do
+ version "1.2.5"
+ url 'http://downloads.typesafe.com/releases/play-1.2.5.zip?raw=true'
+ action :install
+end
+
+
