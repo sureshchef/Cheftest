9e2728037f65738e9a931c6759b3f85f13c49859
sureshchef <suresh.csnrit@gmail.com>
2014-01-15 13:16:09 +0000

    *

--- cookbooks/FastFlowGPU/recipes/default.rb
+++ cookbooks/FastFlowGPU/recipes/default.rb
@@ -142,10 +142,12 @@ execute "configure" do
 end
 
 execute "make" do
+  cwd node['ZMQ']['path']
   command "sudo make"
 end
 
 execute "make install" do
+  cwd node['ZMQ']['path']
   command "sudo make install"
 end
 
